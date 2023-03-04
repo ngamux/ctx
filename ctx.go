8fa24bc6bae8c3a0f4d06dd0b75831e297549797
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-03-04 10:21:11 +0700

    feat(ctx): 🚀 manage req and res from ngamux

--- ctx.go
+++ ctx.go
@@ -1,56 +1,31 @@
 package ctx
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/ngamux/ngamux"
 )
 
 type Context struct {
-	rw http.ResponseWriter
-	r  *http.Request
+	req *ngamux.Request
+	res *ngamux.Response
 }
 
-func New(rw http.ResponseWriter, r *http.Request) *Context {
+func New(w http.ResponseWriter, r *http.Request) *Context {
 	return &Context{
-		rw,
-		r,
+		ngamux.Req(r),
+		ngamux.Res(w),
 	}
 }
 
 // REQUEST
 
-func (c *Context) Request() *http.Request {
-	return c.r
-}
-
-func (c *Context) Context() context.Context {
-	return c.r.Context()
-}
-
-func (c *Context) GetJSON(store interface{}) error {
-	return ngamux.GetJSON(c.r, store)
-}
-
-func (c *Context) GetParam(key string) string {
-	return ngamux.GetParam(c.r, key)
-}
-
-func (c *Context) GetQuery(key string) string {
-	return ngamux.GetQuery(c.r, key)
+func (c *Context) Req() *ngamux.Request {
+	return c.req
 }
 
 // RESPONSE
 
-func (c *Context) Response() http.ResponseWriter {
-	return c.rw
-}
-
-func (c *Context) JSON(status int, data interface{}) error {
-	return ngamux.JSONWithStatus(c.rw, status, data)
-}
-
-func (c *Context) String(status int, data string) error {
-	return ngamux.StringWithStatus(c.rw, status, data)
+func (c *Context) Res() *ngamux.Response {
+	return c.res
 }
